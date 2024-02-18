f70940109eb83e8ded5c4f5b99ceb94bab97a067
Erick Patzan <erickestuardopatzan@gmail.com>
2024-02-18 17:22:15 -0600

    Se agregó el cliente con React

---
+++ client/src/pages/SymbolTable.js
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+
+function SymbolTable() {
+  const symbolTableJSON = localStorage.getItem('symbolTable');
+  const symbolTable = symbolTableJSON ? JSON.parse(symbolTableJSON) : [];
+  const rows = symbolTable.map((symbol, index) => (
+    <tr key={index}>
+      <td>{symbol.id}</td>
+      <td>{symbol.symbolType}</td>
+      <td>{symbol.dataType}</td>
+      <td>{symbol.scope}</td>
+      <td>{symbol.line}</td>
+      <td>{symbol.column}</td>
+    </tr>
+  ));
+
+  useEffect(() => {
+    document.title = 'OLCScript - Tabla de símbolos';
+  });
+
+  return (
+    <div class="container">
+      <h1 style={{ color: 'white', textAlign: 'center', margin: '2%' }}>Tabla de símbolos</h1>
+
+      <table class="table table-dark table-hover">
+        <thead style={{ borderBottom: '1px solid white' }}>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Tipo símbolo</th>
+            <th scope="col">Tipo dato</th>
+            <th scope="col">Ámbito</th>
+            <th scope="col">Línea</th>
+            <th scope="col">Columna</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SymbolTable;
